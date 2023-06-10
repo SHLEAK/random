840efc28fdb420082c7e5dc350a0c05a7105a125
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-06-10 19:55:21 +0900

    Add files via upload

---
+++ pdf-password.py
@@ -0,0 +1,25 @@
+import PyPDF2
+import string
+import itertools
+import concurrent.futures
+import sys
+
+def make(min_length, max_length):
+    character_set = string.ascii_letters + string.digits + string.punctuation
+    for length in range(min_length, max_length + 1):
+        combinations = itertools.product(character_set, repeat=length)
+        for combination in combinations:
+            password = ''.join(combination)
+            yield password
+
+def check(password):
+    file_path = '/Users/lung/Desktop/psychologicalevaluation.pdf'
+    pdf_reader = PyPDF2.PdfReader(file_path)
+    wrong = pdf_reader.decrypt("")
+    if pdf_reader.is_encrypted:
+        if wrong != pdf_reader.decrypt(password):
+            print(f"Password found: {password}")
+            raise KeyboardInterrupt
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(check, make(11, 20))
