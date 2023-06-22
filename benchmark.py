ed90e3d9cd3cd8b0c9db3329dced209380542857
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-06-22 10:39:00 +0900

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,11 @@
 import time
 import datetime
 def bench():
-    start = time.time()
-    n = []
-    i = 0
-    while True:
-        n = [n, n]
-        i += 1  
-        if time.time() - start > 1:
-            return i
-    return None
+    start_time = time.time()
+    iterations = 0
+    while time.time() - start_time < 1:
+        iterations += 1
+    return iterations
 while True:
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     bench_value = bench()
