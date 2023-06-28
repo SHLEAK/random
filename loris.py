66d35b5f9af2cb0b2e066db204c1c33ceb0e830e
Hisu Han <58086385+SHLEAK@users.noreply.github.com>
2023-06-29 05:22:11 +0900

    Update loris.py

--- loris.py
+++ loris.py
@@ -3,7 +3,7 @@ import ssl
 import time
 from urllib.parse import urlparse
 import concurrent.futures
-
+import random
 
 def loris(url, delay):
     parsed_url = urlparse(url)
@@ -36,15 +36,40 @@ def loris(url, delay):
         if use_ssl:
             ssl_socket.close()
         client_socket.close()
-def run_loris():
-    website_url = 'https://www.example.com'
-    character_delay = 2
-    result = loris(website_url, character_delay)
+
+
+def run_loris(delay, url):
+    result = loris(url, delay)
     if result:
-        print("works")
+        return True
     else:
-        print("failed")
-num_threads = 10
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-    futures = [executor.submit(run_loris) for _ in range(num_threads)]
-    concurrent.futures.wait(futures)
+        return False
+
+
+def find_max_delay(num_threads, url):
+    min_delay = 0.0
+    max_delay = 10.0  # Set an upper bound for the delay
+    precision = 0.1  # Adjust precision based on your requirements
+
+    while max_delay - min_delay > precision:
+        delay = (min_delay + max_delay) / 2.0
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+            futures = [executor.submit(run_loris, delay, url) for _ in range(num_threads)]
+            concurrent.futures.wait(futures)
+
+        successful_results = [future.result() for future in futures if future.result()]
+
+        if successful_results:
+            min_delay = delay
+        else:
+            max_delay = delay
+
+    return min_delay
+
+
+num_threads = int(input("Enter the number of threads: "))
+website_url = input("Enter the URL to test: ")
+
+max_delay = find_max_delay(num_threads, website_url)
+print(f"The maximum delay possible is: {max_delay}")
